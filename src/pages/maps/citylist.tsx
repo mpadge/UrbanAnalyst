ef4e49691c4dda6a860e050206a951c5672a6a5c
mpadge <mark.padgham@email.com>
2023-02-15 11:24:44 +0100

    rm coupla console log statements

--- src/pages/maps/citylist.tsx
+++ src/pages/maps/citylist.tsx
@@ -10,8 +10,6 @@ export default function CityList(props: CityListProps) {
         {props.citiesData.map((city, index) =>
           <li key={city.path}>
             <button onClick={() => {
-                console.log("index change to: " + index);
-                console.log("path should change to: " + props.citiesData[index].path);
                 props.handleIdxChange(index);
             }}>
               {city.name}
