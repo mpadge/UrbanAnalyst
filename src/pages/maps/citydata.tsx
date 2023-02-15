6fd54f6635158e24de619e36b5f1dc77660763c5
mpadge <mark.padgham@email.com>
2023-02-15 11:38:18 +0100

    restructure citydata.tsx

--- src/pages/maps/citydata.tsx
+++ src/pages/maps/citydata.tsx
@@ -7,37 +7,39 @@ export const DEFAULT_MAP_CONFIG = {
     }
 }
 
+const berlin = {
+    "name": "berlin",
+    "nameFormatted": "Berlin",
+    "path": "/data/berlin/data.json",
+    "initialViewport": {
+        "latitude": 52.5,
+        "longitude": 13.4,
+        "zoom": 10
+    }
+}
+
+const hamburg = {
+    "name": "hamburg",
+    "nameFormatted": "Hamburg",
+    "path": "/data/hamburg/data.json",
+    "initialViewport": {
+        "latitude": 53.57,
+        "longitude": 10.02,
+        "zoom": 10
+    }
+}
+
+const paris = {
+    "name": "paris",
+    "nameFormatted": "Paris",
+    "path": "/data/paris/data.json",
+    "initialViewport": {
+        "latitude": 48.85,
+        "longitude": 2.39,
+        "zoom": 10
+    }
+}
+
 export const CITY_DATA = {
-    "Data": [
-        {
-            "name": "berlin",
-            "nameFormatted": "Berlin",
-            "path": "/data/berlin/data.json",
-            "initialViewport": {
-                "latitude": 52.5,
-                "longitude": 13.4,
-                "zoom": 10
-            }
-        },
-        {
-            "name": "hamburg",
-            "nameFormatted": "Hamburg",
-            "path": "/data/hamburg/data.json",
-            "initialViewport": {
-                "latitude": 53.57,
-                "longitude": 10.02,
-                "zoom": 10
-            }
-        },
-        {
-            "name": "paris",
-            "nameFormatted": "Paris",
-            "path": "/data/paris/data.json",
-            "initialViewport": {
-                "latitude": 48.85,
-                "longitude": 2.39,
-                "zoom": 10
-            }
-        }
-    ]
+    "Data": [berlin, hamburg, paris]
 };
