a814d776d5f9680053f68f34a57052c589eb5442
mpadge <mark.padgham@email.com>
2023-02-17 14:03:26 +0100

    update all data with full layers

--- src/components/layerlist.tsx
+++ src/components/layerlist.tsx
@@ -7,9 +7,9 @@ import { LayerListProps } from "./interfaces";
 export default function LayerList(props: LayerListProps) {
 
     const options = [
-        { value: 'social', label: 'Social' },
-        { value: 'trans_abs', label: 'Transport (Abs)' },
-        { value: 'trans_rel', label: 'Transport (Rel)' },
+        { value: 'social_index', label: 'Social' },
+        { value: 'transport_abs', label: 'Transport (Abs)' },
+        { value: 'transport_rel', label: 'Transport (Rel)' },
         { value: 'uta_abs', label: 'UTA Index (Abs)' },
         { value: 'uta_rel', label: 'UTA Index (Rel)' }
     ]
