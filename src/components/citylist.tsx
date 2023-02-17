971f83b4f3fe959a883cb0b20f81bd037f76395a
mpadge <mark.padgham@email.com>
2023-02-17 13:45:05 +0100

    use react-select for city selection

--- src/components/citylist.tsx
+++ src/components/citylist.tsx
@@ -1,25 +1,40 @@
 
-import {useState} from 'react';
+import React, { useState } from 'react';
+import Select from 'react-select';
+
 import { CityListProps } from "./interfaces";
 
 export default function CityList(props: CityListProps) {
 
+    const options = props.citiesArray.map((city, index) => ({
+        value: city.name,
+        label: city.nameFormatted,
+        index: index
+    }));
+
+    //const [isClearable, setIsClearable] = useState(true);
+    const [isSearchable, setIsSearchable] = useState(true);
+    const [selected, setSelected] = useState(null);
+
+    const handleChange = (selectedOption: any) => {
+        setSelected(selectedOption);
+        console.log("SELECTOR (value, index) = (" + selectedOption.label + ", " + selectedOption.value + ")")
+        props.handleIdxChange(selectedOption.index);
+        props.handleViewStateChange({...props.citiesArray[selectedOption.index].initialViewState,
+                pitch: 0,
+                bearing: 0 });
+    };
+
   return (
     <section className="city-list">
-      <ul>
-        {props.citiesArray.map((city, index) =>
-          <li key={city.path}>
-            <button onClick={() => {
-                props.handleIdxChange(index);
-                props.handleViewStateChange({...props.citiesArray[index].initialViewState,
-                    pitch: 0,
-                    bearing: 0 });
-            }}>
-              {city.name}
-            </button>
-          </li>
-        )}
-      </ul>
+        <Select
+            options={options}
+            defaultValue={options[0]}
+            name="CitySelector"
+            //isClearable={isClearable}
+            isSearchable={isSearchable}
+            onChange = {handleChange}
+        />
     </section>
   );
 }
--- src/components/control.tsx
+++ src/components/control.tsx
@@ -2,8 +2,7 @@
 import {useState} from 'react';
 import Link from 'next/link'
 import styles from '@/styles/Home.module.css'
-import CityShow from './city-show';
-import CityList from './citylist';
+import CityList from '@/components/citylist';
 import { ControlProps } from "./interfaces";
 
 export default function Control (props: ControlProps) {
@@ -11,8 +10,6 @@ export default function Control (props: ControlProps) {
     const [cityData, setCityData] = useState(props.citiesArray[props.idx]);
 
     const cityNames = props.citiesArray.map((item) => item.name);
-
-    console.log("---->Control: ViewState lon = " + props.viewState.longitude)
 
     return (
         <div id="top-left-container" className={styles.topleft}>
@@ -27,7 +24,7 @@ export default function Control (props: ControlProps) {
         </p>
         </Link>
 
-        <h2>Cities</h2>
+        <h2>City</h2>
         <CityList
             citiesArray={props.citiesArray}
             idx={props.idx}
@@ -35,10 +32,6 @@ export default function Control (props: ControlProps) {
             handleIdxChange={props.handleIdxChange}
             handleViewStateChange={props.handleViewStateChange}
             // onSelect={city => props.handleIdxChange(props.idx)}
-        />
-        <CityShow
-            citiesArray={props.citiesArray}
-            idx={props.idx}
         />
         </div>
         )
