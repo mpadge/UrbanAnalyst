aa5c5a3e5b7a1555ea808a1cae5711a90f37efaf
mpadge <mark.padgham@email.com>
2023-02-21 18:40:57 +0100

    more legend attempts for #2, but doesn't work yet

--- src/components/legend.tsx
+++ src/components/legend.tsx
@@ -35,30 +35,40 @@ export default function Legend (props: LegendProps) {
     const svgRef = React.useRef<SVGSVGElement>(null);
 
     useEffect(() => {
-        var quantize = d3.scaleQuantize<string, string>()
+        var quantize = d3.scaleLinear()
             .domain([ layer_min, layer_max ])
-            .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
+            .range(d3.schemeSet1);
+            //.range([
+            //    "rgb(62, 102, 9)",
+            //    "rgb(145, 104, 37)",
+            //    "rgb(219, 59, 79)",
+            //    "rgb(252, 9, 164)",
+            //    "rgb(252, 164, 255)"
+            //]);
+            //.range(d3.range(9).map(function(i) {
+            //    return "q" + i + "-9"; }));
 
         const svg = d3.select(svgRef.current);
 
         svg.append("g")
-            .attr("class", "legendQuant")
-            .attr("transform", "translate(20,20)");
+            .attr("class", "legendLinear")
+            .attr("transform", "translate(2,20)");
 
         var legend = d3legend.legendColor()
             .labelFormat(d3.format(".2f"))
             .useClass(true)
             .title(this_layer)
             .titleWidth(100)
+            .shapePadding(0)
             .scale(quantize);
 
-        svg.select(".legendQuant")
+        svg.select(".legendLinear")
             .call(legend);
+
     }, [svgRef, this_layer, layer_min, layer_max])
 
     return (
         <div id="bottom-right-container" className={styles.bottomright}>
-        <h2>{this_layer}</h2>
         <svg ref={svgRef} />
 
         </div>
