6f84d47a5900f9b012f42f6fcc1be7740bb63b9c
mpadge <mark.padgham@email.com>
2023-02-23 15:24:19 +0100

    pass props to slider for #6

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -1,17 +1,18 @@
 
-import {useState} from 'react';
+import { useState } from 'react';
 import styles from '@/styles/Home.module.css'
 
 export default function OpacitySlider (props: OpacitySliderProps) {
+
     return (
     <section className="slider">
     <input
         className="opacitySlider"
-        type='range'
-        onChange={props.handleAlphaChange}
+        type="range"
+        onChange={(event) => props.handleAlphaChange(event.target.value)}
         min={0}
-        max={100}
-        step={1}
+        max={1}
+        step={0.1}
         value={props.alpha}
     ></input>
     </section>
