17a0c0a7bb06cb25d1b8eb00cf32798e27fa37f1
mpadge <mark.padgham@email.com>
2023-02-24 12:27:30 +0100

    separate buttons.module.css

--- src/components/maps/buttons.tsx
+++ src/components/maps/buttons.tsx
@@ -1,7 +1,7 @@
 
 import {useState} from 'react';
 import Link from 'next/link'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/buttons.module.css';
 
 export default function Buttons () {
 
