11302dcdc49a13419fb96af9d09b370228e92832
mpadge <mark.padgham@email.com>
2023-02-24 16:01:07 +0100

    clean stats/index for #3

--- src/pages/stats/index.tsx
+++ src/pages/stats/index.tsx
@@ -2,11 +2,8 @@
 import Head from 'next/head'
 import {NextPage} from "next";
 import {useState} from "react";
-import {GeoJsonLayer} from "@deck.gl/layers/typed";
-import {DeckGL} from "@deck.gl/react/typed";
-import {FlyToInterpolator} from "@deck.gl/core/typed";
-import {Map} from "react-map-gl";
 import Buttons from '@/components/stats/buttons';
+import styles from '@/styles/Home.module.css'
 
 import { CITY_DATA } from '@/data/citydata';
 import { CityDataProps, CitiesDataProps, ViewState } from "@/data/interfaces";
@@ -28,7 +25,9 @@ const Page: NextPage<CitiesDataProps> = (props: CitiesDataProps) => {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/uta.ico" />
         </Head>
-        <Buttons />
+        <main className={styles.main}>
+            <Buttons />
+        </main>
         </>
         )
 };
