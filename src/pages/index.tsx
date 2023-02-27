99bff8f5d1a02ebfa85ed53302126f6f1c6cddc9
mpadge <mark.padgham@email.com>
2023-02-27 18:02:29 +0100

    add maps + stats text to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -73,9 +73,52 @@ export default function Home() {
             <h2 className={inter.className}>
               Maps <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
-              Interactive Urban Transport Analyst Maps
+            <p className={styles.cardText}>
+                Click anywhere here to see maps ...
             </p>
+            <p className={styles.cardText}>
+            Interactive maps of all cities analysed to date. Transport values
+            are derived for every street intersection in a city by calculating
+            fastest times taken to travel to every other point in the city using
+            specified single or multiple modes of transport, and calculating the
+            average time needed to travel 10km from each point. The following
+            variables are displayed:
+            </p>
+
+            <ol>
+            <p className={styles.cardText}>
+            <li> "Social" - Representative socio-demographic data such as
+            unemployment rates, deprivation indices, inequality indices, or
+            other indices of social disadvantage.</li>
+            <li> "Transport Absolute" - The average of the time taken to travel
+            10km from every point in a city using multi-modal transporting via
+            any combination of walking, bicycling, or public transport. Values
+            are specified in minutes. Lower values represent faster transport,
+            and are always better.</li>
+            <li> "Transport Relative" - The ratio of absolute travel times
+            described above to times for equivalent journeys taken with private
+            automobile. Ratios less than one represent multi-modal transport
+            being faster than automobile transport. These ratios provide
+            arguably the most direct insight into the propensity or incentive to
+            use public transport: The faster that is in relation to equivalent
+            automobile times, the more likely people will be to use it.</li>
+            <li> "Combined Absolute" - The combination of absolute multi-modal
+            travel times and the social index transformed to the same scale of
+            minutes per 10km of travel. Low values of this index reflect an
+            advantageous combination of good transport provision and low
+            socio-demographic disadvantage. High values indicate regions within
+            a city negatively affected by slow transport and pronounced
+            socio-demographic disadvantage.</li>
+            <li> "Combined Relative" - The combination of multi-modal travel
+            times relative to equivalent automobile times, and the social index
+            transformed to the same scale, so that values of one reflect average
+            social disadvantage and multi-modal travel times the same as
+            equivalent automobile times. Low values reflect low
+            socio-demographic disadvantage, and public transport times being
+            comparable with, or better than, equivalent automobile travel times.
+            </li>
+            </p>
+            </ol>
           </Link>
 
 
@@ -87,8 +130,39 @@ export default function Home() {
             <h2 className={inter.className}>
               Statistics <span>-&gt;</span>
             </h2>
-            <p className={inter.className}>
-              Statistical summaries of UTA Cities
+            <p className={styles.cardText}>
+                Click anywhere here to see stats ...
+            </p>
+            <p className={styles.cardText}>
+            The statistics, or "stats", section shows average values for every
+            UTA city for each variable shown in the maps. This enables the
+            overall values for any city to be compared with values for all other
+            cities. Social indices are not summarised in this section, because
+            they are effectively arbitrary for each city and can not be sensibly
+            combined or compared. The combined indices transform those arbitrary
+            indices to the same scale as the transport layers, and thus do
+            provide direct insight into differences between cities.
+            </p>
+            <h2 className={inter.className}>
+              The UTA Index
+            </h2>
+            <p className={styles.cardText}>
+            The initial statistic on the "stats" page is the "UTA Index", which
+            reflects the main aim of UTA to provide insight into how and where
+            transport systems actively combat social inequality. This index is
+            also calculated in "absolute" and "relative" forms, and reflects the
+            extent to which transport systems work with or against the
+            socio-demographic disadvantage. Cites in which poor transport
+            combines with social disadvantage, while good transport combines
+            with social advantage, will have "Combined" statistics which are
+            more widely distributed than transport indices alone. The UTA index
+            measures the extent to which combination of transport and social
+            indices decreases or increases the overall distribution of social
+            disadvantage alone. As with all other indices, low values are good.
+            Values less than one indicate transport systems effectively
+            counter-balancing socio-demographic disadvantage, while values
+            greater than one indicate transport systems which exacerbate social
+            disadvantage.
             </p>
           </Link>
 
