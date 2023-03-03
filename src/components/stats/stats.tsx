49a69e189cc42f8a5c5de1575d8b7dbd28971753
mpadge <mark.padgham@email.com>
2023-03-03 12:32:57 +0100

    turn barchart sideways

--- src/components/stats/stats.tsx
+++ src/components/stats/stats.tsx
@@ -2,8 +2,30 @@
 import React, {useState, useEffect} from 'react';
 import * as d3 from 'd3';
 import Link from 'next/link'
+import styled from 'styled-components';
 import styles from '@/styles/stats.module.css';
 import { StatsProps } from "@/data/interfaces";
+
+const GroupedBars = styled.g`
+  rect {
+    fill: #ec008b;
+    transition: all 0.2s;
+
+    &:hover {
+      fill: #e46aa7;
+    }
+  }
+`;
+
+const GroupedText = styled.g`
+  text {
+    font-size: 1.2rem;
+  }
+`;
+
+export const Group = styled.g`
+  transform: ${props => `translate(${props.right}px, ${props.top}px)`};
+`;
 
 function useWindowSize() {
 
@@ -43,100 +65,97 @@ export default function Stats (props: StatsProps) {
 
     const [cityData, setCityData] = useState(props.citiesArray[props.idx]);
 
-    function update(svg: any, data: any, width: any, height: any, margin: any) {
-
-        svg.selectAll("rect").remove();
-        svg.selectAll("text").remove();
-        svg.selectAll("line").remove();
-
-        console.log(width + " + " + height)
-
-        // var t = d3.transition()
-        //     .duration(750);
-
-        // X-axis:
-        const cityNames = data.map((item: any) => item.city);
-
-        var x = d3.scaleBand()
-            .range([ 0, width ])
-            .domain(cityNames)
-            .padding(0.2);
-
-        svg.append("g")
-            .attr("transform", "translate(0," + (height - margin.top) + ")")
-            .call(d3.axisBottom(x))
-            .selectAll("text")
-            .attr("transform", "translate(0,20)")
-            .style("font-size", "36px")
-            .style("text-anchor", "end");
-
-        // Y-axis
-        const values = data.map((item: any) => item.value);
-        const ymax = Math.max(...values);
-        var y = d3.scaleLinear()
-            .domain([0, ymax])
-            .range([ height, 0]);
-
-        svg.append("g")
-            .call(d3.axisLeft(y))
-            .attr("transform", "translate(50," + (-margin.top) + ")")
-            .style("font-size", "24px");
-
-        // Bars
-        svg.selectAll("mybar")
-            .data(data)
-            .enter()
-            .append("rect")
-                .attr("x", function(d: any) { return x(d.city); })
-                .attr("y", function(d: any) { return y(d.value); })
-                .attr("transform", "translate(0," + (-margin.top) + ")")
-                .attr("width", x.bandwidth())
-                .attr("height", function(d: any) { return height - y(d.value); })
-                .attr("fill", "#69b3a2");
-    }
-
-    const svgRef = React.useRef<SVGSVGElement>(null);
+    const data = props.citiesArray.map((city, index) => ({
+        city: city.name,
+        value: city.statistics.sd_uta2trans_rel
+    }));
 
     const size = useWindowSize();
 
+    const defaultWidth = 1000;
+    const defaultHeight = 700;
+    var widthTemp = defaultWidth;
+    var heightTemp = defaultHeight;
+    const margin = { top: 50, right: 95, bottom: 60, left: 50 };
+    if (size.width !== null) {
+        widthTemp = Math.min(widthTemp, size.width)
+    }
+    const width = widthTemp;
+    const height = heightTemp;
+    const innerWidth = width - margin.right - margin.left;
+    const innerHeight = height - margin.top - margin.bottom;
+
+    const barsRef = React.useRef<SVGSVGElement>(null);
+    const textRef = React.useRef<SVGSVGElement>(null);
+
     useEffect(() => {
 
-        if (size.width !== null && size.height !== null) {
+        const bars = d3.select(barsRef.current);
+        const text = d3.select(textRef.current);
 
-            var svg = d3.select(svgRef.current);
+        handleDrawBars(bars);
+        handleDrawText(text);
+    }, [data]);
 
-            // Need to resize the svg to current window size:
-            var margin = {top: 80, right: 30, bottom: 100, left: 60};
+    // var t = d3.transition()
+    //     .duration(750);
 
-            const width = size.width - margin.left - margin.right;
-            const height = size.height - margin.top - margin.bottom;
+    // X-axis:
+    const xValue = d => d.value;
+    const xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, xValue)])
+        .range([0, innerWidth])
+        .nice();
 
-            svg.attr("width", width)
-                .attr("height", height);
-                //.attr("transform", "translate(" + margin.left + "," - margin.top + ")");
+    // Y-axis
+    const yValue = d => d.city;
+    const yScale = d3.scaleBand()
+        .domain(data.map(yValue))
+        .range([0, innerHeight])
+        .padding(0.2);
 
-            const cityNames = props.citiesArray.map((item) => item.name);
+    const handleDrawBars = bars => {
+        bars
+          .selectAll('rect')
+          .data(data)
+          .join('rect')
+          .attr('height', yScale.bandwidth())
+          .attr('y', d => yScale(yValue(d)))
+          .transition()
+          .duration(750)
+          .attr('width', d => xScale(xValue(d)));
+    };
 
-            //const x = props.citiesArray.map(city => (city.statistics.uta_rel[0]));
-            //const x = props.citiesArray.map(city => (city.statistics.uta_rel[1]));
+  const handleDrawText = bars => {
+    bars
+      .selectAll('text')
+      .data(data)
+      .join('text')
+      .attr('y', d => yScale(yValue(d)) + yScale.bandwidth() / 1.5)
+      //.text(d => d3.format(d.statistics[thisvar][barData.indx]))
+      //.text(d => d.statistics[thisvar][barData.indx])
+      .text(d => d.city)
+      .attr('x', d => xScale(xValue(d)) + 5)
+      .attr('fill-opacity', 0.8)
+      .transition()
+      .delay(0)
+      .duration(750)
+      .attr('fill-opacity', 0.8);
+  };
 
-            // These are single values only:
-            //const sd_uta2trans_abs = props.citiesArray.map(city => (city.statistics.sd_uta2trans_abs));
-            const yvals = props.citiesArray.map(city => (city.statistics.sd_uta2trans_rel));
-            const data = props.citiesArray.map((city, index) => ({
-                city: city.name,
-                value: city.statistics.sd_uta2trans_rel
-            }));
-
-            update(svg, data, width, height, margin);
-        }
-
-    }, [svgRef, props, size])
 
     return (
             <div id="stats-container" className={styles.statsplot}>
-            <svg ref={svgRef} />
-
+                <svg width={width} height={height}>
+                <Group
+                  x={width / 2}
+                  y={height / 2}
+                  right={margin.right}
+                  top={margin.top}>
+                    <GroupedBars ref={barsRef} />
+                    <GroupedText ref={textRef} />
+                </Group>
+                </svg>
             </div>
            )
 };
