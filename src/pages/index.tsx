d301b30256f70e83576371e57dd39c0d52182572
mpadge <mark.padgham@email.com>
2023-03-07 12:40:41 +0100

    restyle main uta header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,100 +7,98 @@ import styles from '@/styles/Home.module.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>UTA</title>
-        <meta name="description" content="Urban Transport Analyses for the world" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/uta.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Urban Transport Analyst
-          </p>
-          <div>
-            <a
-              href="https://github.com/UrbanAnalyst"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                src="/uta.svg"
-                alt="UTA Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+    return (
+        <>
+        <Head>
+            <title>UTA</title>
+            <meta name="description" content="Urban Transport Analyses for the world" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <link rel="icon" href="/uta.ico" />
+        </Head>
+        <main className={styles.main}>
+            <div className={styles.description}>
+                <p>
+                    <a
+                    href="https://github.com/UrbanAnalyst"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                        Urban Transport Analyst
+                        <Image
+                        src="/uta.svg"
+                        alt="UTA Logo"
+                        className={styles.vercelLogo}
+                        width={100}
+                        height={50}
+                        priority
+                        />
+                    </a>
+                </p>
+            </div>
 
-        <div className={styles.centerBig}>
-          <p className="text-center">
-          An open source platform for analysing and comparing urban transport systems across the world.
-          </p>
-        </div>
+            <div className={styles.centerBig}>
+                <p className="text-center">
+                    An open source platform for analysing and comparing urban transport systems across the world.
+                </p>
+            </div>
 
-        <div className={styles.centerMed}>
-          <p className="text-center">
-          Urban Transport Analyst provides interactive maps of urban transport
-          systems, and relationships of these transport systems to
-          socio-demographic conditions.  UTA also provides statistical summaries
-          of all cities, including representation in the &quot;UTA Index&quot;, which
-          captures the extent to which transport systems effectively
-          counter-balance socio-demographic disadvantage.
-          </p>
+            <div className={styles.centerMed}>
+                <p className="text-center">
+                    Urban Transport Analyst provides interactive maps of urban transport
+                    systems, and relationships of these transport systems to
+                    socio-demographic conditions.  UTA also provides statistical summaries
+                    of all cities, including representation in the &quot;UTA Index&quot;, which
+                    captures the extent to which transport systems effectively
+                    counter-balance socio-demographic disadvantage.
+                </p>
 
-          <p className="text-center">
-          For all maps and statistical summaries, lower values are always
-          better. Transport statistics are based on times needed to travel a
-          given distance, so lower values reflect faster transport.
-          Socio-demographic statistics are metrics of social disadvantage such
-          as unemployment, for which again lower values are better.
-          </p>
-        </div>
+                <p className="text-center">
+                    For all maps and statistical summaries, lower values are always
+                    better. Transport statistics are based on times needed to travel a
+                    given distance, so lower values reflect faster transport.
+                    Socio-demographic statistics are metrics of social disadvantage such
+                    as unemployment, for which again lower values are better.
+                </p>
+            </div>
 
-        <div className={styles.grid}>
+            <div className={styles.grid}>
 
-          <Link
-            href="/maps"
-            className={styles.card}
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Maps <span>-&gt;</span>
-            </h2>
-            <p className={styles.cardText}>
-            Interactive maps of all cities analysed to date. Transport values
-            are derived for every street intersection in a city by calculating
-            fastest times taken to travel to every other point in the city using
-            specified single or multiple modes of transport, and calculating the
-            average time needed to travel 10km from each point.
-            </p>
-          </Link>
+                <Link
+                href="/maps"
+                className={styles.card}
+                rel="noopener noreferrer"
+                >
+                    <h2 className={inter.className}>
+                        Maps <span>-&gt;</span>
+                    </h2>
+                    <p className={styles.cardText}>
+                        Interactive maps of all cities analysed to date. Transport values
+                        are derived for every street intersection in a city by calculating
+                        fastest times taken to travel to every other point in the city using
+                        specified single or multiple modes of transport, and calculating the
+                        average time needed to travel 10km from each point.
+                    </p>
+                </Link>
 
 
-          <Link
-            href="/stats"
-            className={styles.card}
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Statistics <span>-&gt;</span>
-            </h2>
-            <p className={styles.cardText}>
-            The statistics, or &quot;stats&quot;, section shows average values for every
-            UTA city for each variable shown in the maps. This enables the
-            overall values for any city to be compared with values for all other
-            cities.
-            </p>
-          </Link>
+                <Link
+                href="/stats"
+                className={styles.card}
+                rel="noopener noreferrer"
+                >
+                    <h2 className={inter.className}>
+                        Statistics <span>-&gt;</span>
+                    </h2>
+                    <p className={styles.cardText}>
+                        The statistics, or &quot;stats&quot;, section shows average values for every
+                        UTA city for each variable shown in the maps. This enables the
+                        overall values for any city to be compared with values for all other
+                        cities.
+                    </p>
+                </Link>
 
-        </div>
-      </main>
+            </div>
+        </main>
     </>
-  )
+    )
 }
