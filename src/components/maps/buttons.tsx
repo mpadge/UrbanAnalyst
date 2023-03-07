d20921990434901d41a4851246c18f93d999b870
mpadge <mark.padgham@email.com>
2023-03-07 21:04:21 +0100

    fix buttons to be single class, right flex aligned

--- src/components/maps/buttons.tsx
+++ src/components/maps/buttons.tsx
@@ -7,24 +7,26 @@ export default function Buttons () {
 
     return (
         <>
-        <div id="top-right-container" className={styles.topright90}>
-        <Link
-            href="/"
-            className={styles.card}
-            rel="noopener noreferrer"
-        >
-        <p> Home </p>
-        </Link>
-        </div>
+        <div id="top-right-container" className={styles.buttons}>
+            <div id="home-button">
+                <Link
+                    href="/"
+                    className={styles.card}
+                    rel="noopener noreferrer"
+                >
+                <p> Home </p>
+                </Link>
+            </div>
 
-        <div id="top-right-container" className={styles.topright80}>
-        <Link
-            href="/stats"
-            className={styles.card}
-            rel="noopener noreferrer"
-        >
-        <p> Stats </p>
-        </Link>
+            <div id="stats-button">
+                <Link
+                    href="/stats"
+                    className={styles.card}
+                    rel="noopener noreferrer"
+                >
+                <p> Stats </p>
+                </Link>
+            </div>
         </div>
         </>
         )
--- src/components/stats/buttons.tsx
+++ src/components/stats/buttons.tsx
@@ -7,24 +7,26 @@ export default function Buttons () {
 
     return (
         <>
-        <div id="top-right-container" className={styles.topright90}>
-        <Link
-            href="/"
-            className={styles.card}
-            rel="noopener noreferrer"
-        >
-        <p> Home </p>
-        </Link>
-        </div>
+        <div id="top-right-container" className={styles.buttons}>
+            <div id="home button">
+                <Link
+                    href="/"
+                    className={styles.card}
+                    rel="noopener noreferrer"
+                >
+                <p> Home </p>
+                </Link>
+            </div>
 
-        <div id="top-right-container" className={styles.topright80}>
-        <Link
-            href="/maps"
-            className={styles.card}
-            rel="noopener noreferrer"
-        >
-        <p> Maps </p>
-        </Link>
+            <div id="maps button">
+                <Link
+                    href="/maps"
+                    className={styles.card}
+                    rel="noopener noreferrer"
+                >
+                <p> Maps </p>
+                </Link>
+            </div>
         </div>
         </>
         )
