7d530c5717db0fa5e98f771522f7c8d870e16406
mpadge <mark.padgham@email.com>
2023-05-31 10:54:40 +0200

    mv statsheadingtext fn to components/heading_text.tsx for #28

---
+++ src/components/heading_text.tsx
@@ -0,0 +1,31 @@
+
+export default function StatsHeadingText (layer: string) {
+
+    var heading: string = "";
+
+    if (layer.replace("\_", "").substring(0,6) === "social") {
+        heading = "Social Index";
+    } else if (layer.replace("\_", "") === "timesrel") {
+        heading = "Transport Relative";
+    } else if (layer.replace("\_", "") === "timesabs") {
+        heading = "Transport Absolute";
+    } else if (layer === "transfers") {
+        heading = "Num. Transfers";
+    } else if (layer === "intervals") {
+        heading = "Transport Interval";
+    } else if (layer === "transport") {
+        heading = "Transport Combined";
+    } else if (layer === "popdens") {
+        heading = "Population";
+    } else if (layer.replace("\_", "") === "schooldist") {
+        heading = "School Distance";
+    } else if (layer.replace("\_", "").substring(0,4) === "bike") {
+        heading = "Bicycle Index";
+    } else if (layer === "natural") {
+        heading = "Nature Index";
+    } else if (layer === "parking") {
+        heading = "Parking";
+    }
+
+    return heading;
+}
