9458d04c3f0a3f2eafd3fcd8e65690f595e7cf24
mpadge <mark.padgham@email.com>
2023-05-31 10:54:40 +0200

    mv statsheadingtext fn to components/heading_text.tsx for #28

--- src/components/stats/stats.tsx
+++ src/components/stats/stats.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import styled from 'styled-components';
 import styles from '@/styles/stats.module.css';
 import { CityDataProps } from "@/data/interfaces";
+import StatsHeadingText from "@/components/heading_text";
 
 interface StatsProps {
     idx: number,
@@ -109,37 +110,6 @@ function useWindowSize() {
     }, []); // Empty array ensures that effect is only run on mount
 
     return windowSize;
-}
-
-function StatsHeadingText (layer: string) {
-
-    var heading: string = "";
-
-    if (layer.replace("\_", "").substring(0,6) === "social") {
-        heading = "Social Index";
-    } else if (layer.replace("\_", "") === "timesrel") {
-        heading = "Transport Relative";
-    } else if (layer.replace("\_", "") === "timesabs") {
-        heading = "Transport Absolute";
-    } else if (layer === "transfers") {
-        heading = "Num. Transfers";
-    } else if (layer === "intervals") {
-        heading = "Transport Interval";
-    } else if (layer === "transport") {
-        heading = "Transport Combined";
-    } else if (layer === "popdens") {
-        heading = "Population";
-    } else if (layer.replace("\_", "") === "schooldist") {
-        heading = "School Distance";
-    } else if (layer.replace("\_", "").substring(0,4) === "bike") {
-        heading = "Bicycle Index";
-    } else if (layer === "natural") {
-        heading = "Nature Index";
-    } else if (layer === "parking") {
-        heading = "Parking";
-    }
-
-    return heading;
 }
 
 export default function Stats (props: StatsProps) {
