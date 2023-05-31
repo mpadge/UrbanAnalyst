a2bc20bf78bc175299d377c6432bbe5035ce7b53
mpadge <mark.padgham@email.com>
2023-05-31 14:47:12 +0200

    set map explain layer to citydata field for social_index

--- src/components/maps/explain-layer.tsx
+++ src/components/maps/explain-layer.tsx
@@ -17,7 +17,12 @@ export default function ExplainButton (props: MapsExplainProps) {
 
     const meanVarIndex = 0;
     const paired_keys: string[] = Object.keys(props.citiesArray[props.idx].dataIntervalsPaired);
-    const layer_text = GetLayerText(props.layer, props.layer2, props.numLayers, meanVarIndex, paired_keys);
+    const layer_temp = GetLayerText(props.layer, props.layer2, props.numLayers, meanVarIndex, paired_keys);
+    // But set layer text to city data "social_index" field if that single layer
+    // is selected:
+    const layer_is_paired: boolean = paired_keys.includes(layer_temp);
+    const layer_text = !layer_is_paired && props.layer == "social_index" ?
+        props.citiesArray[props.idx].social_index : layer_temp;
 
     return (
         <div id="divinfo" style={{display: props.explain?"":"none"}} >
