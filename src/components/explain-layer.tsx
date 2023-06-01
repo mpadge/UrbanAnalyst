cb057a195da5a75512757f986034471f164cbc88
mpadge <mark.padgham@email.com>
2023-06-01 14:29:12 +0200

    extend all explanations of single layers

--- src/components/explain-layer.tsx
+++ src/components/explain-layer.tsx
@@ -18,37 +18,37 @@ export function GetLayerText(layer: string, layer2: string, numLayers: string, m
         "social": ["Representative socio-demographic data such as unemployment rates, deprivation indices, inequality indices, or other indices of social disadvantage."],
 
         "timesabs": [
-            "The average of the fastest times taken to travel 10km from every point in a city using multi-modal transporting via any combination of walking, bicycling, or public transport. Values are specified in minutes. Lower values represent faster transport, and are always better.",
+            "The average of the fastest times taken to travel 10km from every point in a city using multi-modal transport via any combination of walking, bicycling, or public transport. Values are specified in minutes. Lower values represent faster transport, and are always better. Values are also weighted by local population densities, so represent average times of fastest journeys per person within each polygon.",
             "Variation in the average of the fastest times taken to travel 10km from every point in a city using multi-modal transporting via any combination of walking, bicycling, or public transport. Values are on a scale of minutes. Lower values represent less variation in travel times across the city."
             ],
 
         "timesrel": [
-            "The ratio of absolute travel times for any combinations of walking, cycling, and public transport to times for equivalent journeys taken solely with private automobile. Ratios less than one represent multi-modal transport being faster than automobile transport. These ratios provide arguably the most direct insight into the propensity or incentive to use public transport: Lower values mean that faster public transport is faster relation to equivalent automobile times, meaning people will be more likely to use it.",
+            "The ratio of absolute travel times for distances of 10 kilometres using any combinations of walking, cycling, and public transport to times for equivalent journeys taken solely with private automobile. Ratios less than one represent multi-modal transport being faster than automobile transport. Values are weighted by local population densities, so represent average relative times of fastest journeys per person within each polygon. These ratios provide arguably the most direct insight into the propensity or incentive to use public transport: Lower values mean that faster public transport is faster relation to equivalent automobile times, meaning people will be more likely to use it.",
             "Variation in the ratio of absolute travel times for any combinations of walking, cycling, and public transport to times for equivalent journeys taken solely with private automobile. Lower values imply lower variation across a city in the ratios of multi-modal versus private automobile travel times."
         ],
         "transfers": [
-            "Number of Transfers per 10km of travel."
+            "Numbers of transfers for the public transport section of multi-modal journeys per 10km of travel. Values are averaged for each point of origin over all possible journeys out to 10km, with values within polygons being averages over all points or origin with each polygon. Values are weighted by local population densities, so represent average numbers of transfers per person within each polygon."
         ],
         "intervals": [
-            "Interval between public transport services (in minutes)."
+            "Intervals (in minutes) between public the fastest public transport trip and the subsequent equivalent trips (which may be slower than the first trip). Intervals are averaged for each point of origin over all possible journeys out to 10km, with values within polygons being averages over all points or origin with each polygon. Values are weighted by local population densities, so represent average intervals per person within each polygon. This provides a direct measure of the frequency of public transport services."
         ],
         "transport": [
-            "Combined measure of tranpsport time times number of transfers times intervals between services."
+            "Combined measure of transport time times number of transfers times intervals between services. Values may be directly compared between different locations, and different cities. Lower values indicate public transport that is fast, frequent, and requires few transfers."
         ],
         "popdens": [
-            "Population density"
+            "Population density, in units of thousands of people per square kilometre."
         ],
         "schooldist": [
-            "Distance to nearest school (in kilometres)"
+            "Average distance to nearest school (in kilometres). Single values are calculated for every street junction within each polygon, as the distance to the nearest school. Values for each polygon are then averaged over these single distances for every street junction within the polygon, weighted by population density so that final values are average distance per person."
         ],
         "bike": [
-            "Bicycle Index"
+            "Bicycle Index, measured as one minus the proportion of all journeys from each street junction within each polygon out to a distance of 5km travelling along dedicated bicycle infrastructure. Values of zero imply all journeys on dedicated bicycle ways, while values of one imply all journeys directly alongside automobiles, with no bicycle infrastructure at all. Unlike static aggregations of lengths of dedicated bicycle infrastructure, this index provides direct insight into the proportions of all journeys actually spent cycling along appropriate infrastructure."
         ],
         "natural": [
-            "Nature Index"
+            "Index of accessibility to natural spaces, measured as one minus the proportion of all walking journeys from each street junction within each polygon out to a distance of 2km travelling through or alongside natural spaces. Natural spaces include any publicly accessible areas with natural surfaces, as well as all bodies of water with publicly accessible ways along the sides. Unlike static aggregations of areas of natural space, this index provides direct insight into the proportions of all journeys actually spent walking within or alongside natural spaces."
         ],
         "parking": [
-            "Parking index"
+            "Parking index, as a (logarithmically) re-scaled measure of numbers of parking spaces per unit building volume. Lower values imply fewer available parking spaces for a given volume of building. Values for each street junction within a polygon are averaged across all nearby parking spaces, inversely weighted by distance. Each point is the described by a single measure of average number of nearby spaces per volume of nearby buildings. Values within each polygon are then population-density average values of these, providing final measures of average parking availability per person."
         ],
 
         "social_timesabs": [
@@ -82,31 +82,31 @@ export function GetLayerText(layer: string, layer2: string, numLayers: string, m
             "Relative travel times multiplied by social index (rescaled to average of one)."
         ],
         "timesabs_transfers": [
-            "Absoulte travel times multiplied by numbers of transfers."
+            "Absolute travel times multiplied by numbers of transfers."
         ],
         "timesabs_intervals": [
-            "Absoulte travel times multiplied by intervals between consecutive services."
+            "Absolute travel times multiplied by intervals between consecutive services."
         ],
         "timesabs_transport": [
-            "Absoulte travel times multiplied by compound transport index. These values will generally not mean anything."
+            "Absolute travel times multiplied by compound transport index. These values will generally not mean anything."
         ],
         "timesabs_popdens": [
-            "Absoulte travel times multiplied by population densities.."
+            "Absolute travel times multiplied by population densities.."
         ],
         "timesabs_schooldist": [
-            "Absoulte travel times multiplied by distances to nearest schools."
+            "Absolute travel times multiplied by distances to nearest schools."
         ],
         "timesabs_bike": [
-            "Absoulte travel times multiplied by bicycle index."
+            "Absolute travel times multiplied by bicycle index."
         ],
         "timesabs_natural": [
-            "Absoulte travel times multiplied by index of accessibility to natural spaces."
+            "Absolute travel times multiplied by index of accessibility to natural spaces."
         ],
         "timesabs_parking": [
-            "Absoulte travel times multiplied by parking index."
+            "Absolute travel times multiplied by parking index."
         ],
         "timesabs_social": [
-            "Absoulte travel times multiplied by social index (rescaled to average of one)."
+            "Absolute travel times multiplied by social index (rescaled to average of one)."
         ],
         "transfers_intervals": [
             "Numbers of transfers multiplied by intervals between consecutive services."
