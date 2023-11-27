44c2c5d6599c14b9a1d5312c7c81b53ff425a900
mpadge <mark.padgham@email.com>
2023-11-27 14:35:28 +0100

    build wasm in 'pkg' sub-dir for #49

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack(config, { isServer, dev }) {
+    // Use the client static directory in the server bundle and prod mode
+    // Fixes `Error occurred prerendering page "/"`
+    config.output.webassemblyModuleFilename =
+      isServer && !dev
+        ? '../static/pkg/[modulehash].wasm'
+        : 'static/pkg/[modulehash].wasm'
+
+    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+    config.experiments = { ...config.experiments, asyncWebAssembly: true }
+
+    return config
+  },
+}
 
 module.exports = nextConfig
