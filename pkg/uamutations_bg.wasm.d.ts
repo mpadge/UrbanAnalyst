602002524acf3229ed3a142e87ec9667a80b0edc
mpadge <mark.padgham@email.com>
2023-11-27 14:42:15 +0100

    add wasm pkg folder + compiled binary for #49

---
+++ pkg/uamutations_bg.wasm.d.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function uamutate(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number): number;
+export function get_result_len(): number;
