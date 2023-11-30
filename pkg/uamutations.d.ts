d0f28f101085359014c318f4068fc42c5df2ebce
mpadge <mark.padgham@email.com>
2023-11-30 13:46:41 +0100

    manually edit pkg/*.d.ts files to reduce 6 -> 5 vars
    No idea what's wrong there, but this was necessary because the interfaces
    were being auto-generated with 6 instead of 5 arguments.

--- pkg/uamutations.d.ts
+++ pkg/uamutations.d.ts
@@ -39,7 +39,7 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
-  readonly uamutate: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+  readonly uamutate: (a: number, b: number, c: number, d: number, e: number) => void;
   readonly __wbindgen_malloc: (a: number, b: number) => number;
   readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
   readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
