a7ef706313b9993d86765d43d0c56001ec517fdb
mpadge <mark.padgham@email.com>
2023-12-16 10:45:31 +0100

    clean map-mutate for #49

--- src/components/maps/map-mutate.tsx
+++ src/components/maps/map-mutate.tsx
@@ -1,15 +1,6 @@
-import { NextPage } from "next";
-import { useEffect, useState, Suspense } from "react";
-import { GeoJsonLayer } from "@deck.gl/layers/typed";
-import { DeckGL } from "@deck.gl/react/typed";
-import { Map } from "react-map-gl";
-import * as d3 from 'd3';
 import dynamic from 'next/dynamic'
 
-import * as wasm_js from '@/../pkg/uamutations.js';
 import BindGenComponent from '@/components/maps/map-mutateComponent';
-
-const MapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
 interface BindGenProps {
     filename1: string
