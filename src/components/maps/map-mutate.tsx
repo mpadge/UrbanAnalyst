6fa73d50a7b815c64ca29c7dbd6c2875c14d7cd8
mpadge <mark.padgham@email.com>
2023-12-21 07:07:10 +0100

    add comments at start of map-mutate #49

--- src/components/maps/map-mutate.tsx
+++ src/components/maps/map-mutate.tsx
@@ -1,3 +1,6 @@
+// This just wraps a nextjs dynamic call around the actual 'BindGenComponent'
+// which loads and calls the WASM binary.
+
 import dynamic from 'next/dynamic'
 
 import BindGenComponent from '@/components/maps/map-mutateComponent';
