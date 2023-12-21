3d734bf0c2bc09fb971f051ea6ba393d18089675
mpadge <mark.padgham@email.com>
2023-12-21 03:39:18 +0100

    split mapLayer code into separate component

---
+++ src/components/maps/mapLayer.tsx
@@ -0,0 +1,96 @@
+import { NextPage } from "next";
+import { useState, Suspense } from "react";
+import { GeoJsonLayer } from "@deck.gl/layers/typed";
+import { FlyToInterpolator } from "@deck.gl/core/typed";
+import * as d3 from 'd3';
+import 'd3-scale-chromatic';
+
+import { ViewState, CityDataProps } from "@/data/interfaces";
+
+interface MapProps {
+    idx: number,
+    layer: string,
+    layer2: string,
+    numLayers: string,
+    alpha: number,
+    viewState: ViewState,
+    citiesArray: CityDataProps[],
+    handleAlphaChange: (pAlpha: number) => void,
+    handleViewStateChange: (pViewState: ViewState) => void,
+    handleLayerChange: (layer: string) => void
+    handleLayer2Change: (layer2: string) => void
+}
+
+
+export default function UTAMapLayer (props: MapProps) {
+
+    const mapPath1 = props.citiesArray[props.idx].path;
+    const mapPath2 = mapPath1.replace("data\.json", "data2.json");
+
+    const [viewState, setViewState] = useState({
+        ...props.viewState,
+        transitionDuration: 2000,
+        transitionInterpolator: new FlyToInterpolator()
+    });
+
+    const layer1: string = props.layer.replace("\_", "").replace("index", "");
+    const layer2: string = props.layer2.replace("\_", "").replace("index", "");
+    const paired_keys = Object.keys(props.citiesArray[props.idx].dataRangesPaired);
+
+    const these_layers =
+        paired_keys.includes(layer1 + "_" + layer2) ?
+        layer1 + "_" + layer2 : layer2 + "_" + layer1;
+    const dual_layers: boolean = paired_keys.includes(these_layers);
+
+    const this_layer: string = props.numLayers == "Paired" && dual_layers ?
+        these_layers : props.layer;
+
+    const layer_min = props.numLayers == "Paired" && dual_layers ?
+            props.citiesArray[props.idx].dataRangesPaired[these_layers as string][0] :
+        props.citiesArray[props.idx].dataRanges[this_layer as string][0];
+    const layer_max = props.numLayers == "Paired" && dual_layers ?
+            props.citiesArray[props.idx].dataRangesPaired[these_layers as string][1] :
+        props.citiesArray[props.idx].dataRanges[this_layer as string][1];
+
+    // palettes:
+    // https://github.com/d3/d3-scale-chromatic
+    var Color = d3.scaleSequential().domain([ layer_min, layer_max ])
+        //.interpolator(d3.interpolateMagma)
+        //.interpolator(d3.interpolateCividis)
+        .interpolator(d3.interpolateViridis)
+
+    const mapPath: string = props.numLayers == "Paired" && dual_layers ? mapPath2 : mapPath1;
+
+    const layers = [
+        new GeoJsonLayer({
+            id: 'polygon-layer',
+            data: mapPath,
+            filled: true,
+            stroked: true,
+            getLineWidth: 10,
+            getLineColor: [122, 122, 122],
+            getFillColor: d => {
+                var layerval = Math.max (layer_min, Math.min (layer_max, d.properties?.[this_layer]));
+                if (isNaN(layerval)) {
+                    layerval = layer_min;
+                }
+                // Invert the palette:
+                layerval = layer_min + (layer_max - layerval);
+                const layerarr: any = d3.color(Color(layerval));
+                var red = 0, green = 0, blue = 0;
+                if (layerarr) {
+                    red = layerarr.r;
+                    green = layerarr.g;
+                    blue = layerarr.b;
+                }
+                return [red, green, blue]
+                },
+            opacity: 1 - props.alpha,
+            updateTriggers: {
+                getFillColor: [this_layer]
+            }
+            })
+        ]
+
+    return layers
+};
