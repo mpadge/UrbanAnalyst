8a4ba26cd1b1b035831dd64e028481d57108d4bd
mpadge <mark.padgham@email.com>
2024-01-18 16:14:31 +0100

    clean mutateComponent for #53 #58

--- src/components/maps/map-mutateComponent.tsx
+++ src/components/maps/map-mutateComponent.tsx
@@ -39,15 +39,7 @@ const JSONObjectSize = (obj: any) => {
     return numItems;
 }
 
-async function getEncryptedData(city: string) {
-
-    const path = '/data/' + city + '/dataraw.enc';
-    const encryptedData = await fetch(path);
-    const arrayBuffer = await encryptedData.arrayBuffer();
-
-    const ivPath = '/data/' + city + '/iv.txt';
-    const ivResponse = await fetch(ivPath);
-    const iv = await ivResponse.text().then(text => text.trim());
+async function getGHData(city: string) {
 
     const response = await fetch(`/api/gh?city=${city}`);
     if (!response.ok) {
@@ -73,14 +65,10 @@ const MapMutateComponent = (props: MutateProps) => {
     // and store as 'data1', 'data2':
     useEffect(() => {
         const loadData = async () => {
-            const data1 = await getEncryptedData(props.city);
+            const data1 = await getGHData(props.city);
             setData1(data1);
-            // const json1 = JSON.parse(data1);
-            // setData1(json1);
 
-            const data2 = await getEncryptedData('paris');
-            // const json2 = JSON.parse(data2);
-            // setData2(json2);
+            const data2 = await getGHData('paris');
             setData2(data2);
         };
 
