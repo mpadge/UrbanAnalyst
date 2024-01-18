398552821f5d5dc6c7222201c5f3ce9e0da2e5af
mpadge <mark.padgham@email.com>
2024-01-18 11:02:53 +0100

    fix next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,25 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  webpack(config, { isServer, dev }) {
-    // Use the client static directory in the server bundle and prod mode
-    // Fixes `Error occurred prerendering page "/"`
-    config.output.webassemblyModuleFilename =
-      isServer && !dev
-        ? '../static/pkg/[modulehash].wasm'
-        : 'static/pkg/[modulehash].wasm'
-
-    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-    config.experiments = { ...config.experiments, asyncWebAssembly: true }
-
-    return config
-  },
-  experimental: {
-    serverActions: {
-      bodySizeLimit: '20mb',
+    experimental: {
+        // serverActions: true,
+        serverActions: {
+            bodySizeLimits: '20mb',
+        },
     },
-  }
+    webpack(config, { isServer, dev }) {
+        // Use the client static directory in the server bundle and prod mode
+        // Fixes `Error occurred prerendering page "/"`
+        config.output.webassemblyModuleFilename =
+            isServer && !dev
+            ? '../static/pkg/[modulehash].wasm'
+            : 'static/pkg/[modulehash].wasm'
+
+        // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+        config.experiments = { ...config.experiments, asyncWebAssembly: true }
+
+        return config
+    },
 }
 
 module.exports = nextConfig
