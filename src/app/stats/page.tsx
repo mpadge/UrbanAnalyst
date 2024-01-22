9aa055af14934b1014ec44775649b9aea4d6b948
mpadge <mark.padgham@email.com>
2024-01-22 11:15:07 +0100

    split stats/page -> components/stats/statsPage to add metadata #54

--- src/app/stats/page.tsx
+++ src/app/stats/page.tsx
@@ -1,99 +1,24 @@
-"use client"
 
-import Head from 'next/head'
-import { NextPage } from "next";
-import { useState } from "react";
-import Control from '@/components/stats/control';
-import Buttons from '@/components/buttons2';
-import BarChart from '@/components/stats/stats';
-import styles from '@/styles/Home.module.css'
+import type { Metadata, Viewport } from 'next';
 
-import { CITY_DATA } from '@/data/citydata';
+import StatsPage from '@/components/stats/statsPage';
 
-const buttonProps = {
-    first: "home",
-    second: "maps"
+export const metadata: Metadata = {
+    title: 'UA',
+    description: 'Stats for Urban Analyst',
+    icons: '/ua.ico',
+}
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1.0,
 }
 
 export default function Home() {
 
-    const [idx, setIdx] = useState(0);
-    const [cityData, setCityData] = useState(CITY_DATA.citiesArray[idx]);
-
-    const [layer, setLayer] = useState("social_index"); // options[0] in layerlist.tsx
-    const [layer2, setLayer2] = useState("");
-    const [numLayers, setNumLayers] = useState("Single");
-    const numLayersOptions = ["Single", "Paired"];
-
-    const [explain, setExplain] = useState(false);
-    const [sortOpt, setSortOpt] = useState("increasing");
-    const [meanVals, setMeanVals] = useState(true);
-
-    const handleIdxChange = (idx: number) => {
-        setIdx(idx);
-    }
-    const handleLayerChange = (layer: string) => {
-        setLayer(layer);
-    }
-    const handleLayer2Change = (layer2: string) => {
-        setLayer2(layer2);
-    }
-    const handleNumLayersChange = (numLayers: string) => {
-        setNumLayers(numLayers);
-    }
-    const handleExplainChange = (e: any) => {
-        setExplain(!explain);
-    }
-    const handleSortChange = (sortOpt: string) => {
-        setSortOpt (sortOpt)
-    }
-    const handleMeanChange = (e: any) => {
-        setMeanVals(!meanVals);
-    }
-
-    const buttons = {
-        first: "home",
-        second: "maps"
-    }
-    
     return (
         <>
-        <Head>
-            <title>UA Stats</title>
-            <meta name="description" content="Stats for Urban Analyst" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <link rel="icon" href="/ua.ico" />
-        </Head>
-        <main className={styles.main}>
-            
-            <BarChart
-                idx = {idx}
-                layer1 = {layer}
-                layer2 = {layer2}
-                numLayers = {numLayers}
-                meanVals = {meanVals}
-                sortOpt = {sortOpt}
-                citiesArray = {CITY_DATA.citiesArray}
-            />
-            <Control
-                idx = {idx}
-                layer = {layer}
-                layer2 = {layer2}
-                numLayers = {numLayers}
-                numLayersOptions = {numLayersOptions}
-                meanVals = {meanVals}
-                explain = {explain}
-                sortOpt = {sortOpt}
-                citiesArray = {CITY_DATA.citiesArray}
-                handleLayerChange = {handleLayerChange}
-                handleLayer2Change = {handleLayer2Change}
-                handleNumLayersChange = {handleNumLayersChange}
-                handleMeanChange = {handleMeanChange}
-                handleSortChange = {handleSortChange}
-                handleExplainChange = {handleExplainChange}
-            />
-            <Buttons buttons={buttons} />
-        </main>
+        <StatsPage />
         </>
     )
 }
---
+++ src/components/stats/statsPage.tsx
@@ -0,0 +1,92 @@
+"use client"
+
+import { NextPage } from "next";
+import { useState } from "react";
+import Control from '@/components/stats/control';
+import Buttons from '@/components/buttons2';
+import BarChart from '@/components/stats/stats';
+import styles from '@/styles/Home.module.css'
+
+import { CITY_DATA } from '@/data/citydata';
+
+const buttonProps = {
+    first: "home",
+    second: "maps"
+}
+
+export default function Home() {
+
+    const [idx, setIdx] = useState(0);
+    const [cityData, setCityData] = useState(CITY_DATA.citiesArray[idx]);
+
+    const [layer, setLayer] = useState("social_index"); // options[0] in layerlist.tsx
+    const [layer2, setLayer2] = useState("");
+    const [numLayers, setNumLayers] = useState("Single");
+    const numLayersOptions = ["Single", "Paired"];
+
+    const [explain, setExplain] = useState(false);
+    const [sortOpt, setSortOpt] = useState("increasing");
+    const [meanVals, setMeanVals] = useState(true);
+
+    const handleIdxChange = (idx: number) => {
+        setIdx(idx);
+    }
+    const handleLayerChange = (layer: string) => {
+        setLayer(layer);
+    }
+    const handleLayer2Change = (layer2: string) => {
+        setLayer2(layer2);
+    }
+    const handleNumLayersChange = (numLayers: string) => {
+        setNumLayers(numLayers);
+    }
+    const handleExplainChange = (e: any) => {
+        setExplain(!explain);
+    }
+    const handleSortChange = (sortOpt: string) => {
+        setSortOpt (sortOpt)
+    }
+    const handleMeanChange = (e: any) => {
+        setMeanVals(!meanVals);
+    }
+
+    const buttons = {
+        first: "home",
+        second: "maps"
+    }
+
+    return (
+        <>
+        <main className={styles.main}>
+
+            <BarChart
+                idx = {idx}
+                layer1 = {layer}
+                layer2 = {layer2}
+                numLayers = {numLayers}
+                meanVals = {meanVals}
+                sortOpt = {sortOpt}
+                citiesArray = {CITY_DATA.citiesArray}
+            />
+            <Control
+                idx = {idx}
+                layer = {layer}
+                layer2 = {layer2}
+                numLayers = {numLayers}
+                numLayersOptions = {numLayersOptions}
+                meanVals = {meanVals}
+                explain = {explain}
+                sortOpt = {sortOpt}
+                citiesArray = {CITY_DATA.citiesArray}
+                handleLayerChange = {handleLayerChange}
+                handleLayer2Change = {handleLayer2Change}
+                handleNumLayersChange = {handleNumLayersChange}
+                handleMeanChange = {handleMeanChange}
+                handleSortChange = {handleSortChange}
+                handleExplainChange = {handleExplainChange}
+            />
+            <Buttons buttons={buttons} />
+        </main>
+        </>
+    )
+}
