a674b4ae9da38b13c8c9f3a8225ed384047ed152
mpadge <mark.padgham@email.com>
2024-01-23 14:21:45 +0100

    add react state vars to transform page for #61

--- src/components/transform/mapTransformPage.tsx
+++ src/components/transform/mapTransformPage.tsx
@@ -16,14 +16,8 @@ import { ViewState } from "@/data/interfaces";
 
 export default function MapTransformPage() {
 
-    const idx = 0;
-    const varnames = ["bike_index", "social_index"];
-    const nentries = 1000;
-    const mapPath = "/data/berlin/data.json";
-    const alpha = 0.7;
-
-    const layer_name = varnames[0];
-
+    const [idx, setIdx] = useState(0);
+    const [cityData, setCityData] = useState(CITY_DATA.citiesArray[idx]);
     const [viewState, setViewState] = useState({
         ...CITY_DATA.citiesArray[idx].initialViewState,
         pitch: 0,
@@ -31,6 +25,28 @@ export default function MapTransformPage() {
         transitionDuration: 2000,
         transitionInterpolator: new FlyToInterpolator()
     });
+    const [layer, setLayer] = useState("social_index");
+    const [alpha, setAlpha] = useState(0.5);
+
+    const handleIdxChange = (idx: number) => {
+        setIdx(idx);
+    }
+    const handleAlphaChange = (alpha: number) => {
+        setAlpha(alpha);
+    }
+    const handleViewStateChange = (pViewState: any) => {
+        setViewState((prevViewState) => { return { ...prevViewState, ...pViewState }; });
+        //setViewState(pViewState);
+    }
+    const handleLayerChange = (layer: string) => {
+        setLayer(layer);
+    }
+
+    const varnames = ["bike_index", "social_index"];
+    const nentries = 1000;
+    const mapPath = "/data/berlin/data.json";
+
+    const layer_name = varnames[0];
 
     const [layerMin, setLayerMin] = useState<number>(0);
     const [layerMax, setLayerMax] = useState<number>(0);
@@ -40,7 +56,7 @@ export default function MapTransformPage() {
     const handleLayerMaxChange = (layerMax: number) => {
         setLayerMax(layerMax);
     }
-    const [layerName, setLayerName] = useState<string>("social_index");
+    const [layerName, setLayerName] = useState<string>("bike_index");
 
     return (
         <>
