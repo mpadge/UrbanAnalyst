5b0de2d8504dff64e4f2a067cd66c66764ffc8aa
mpadge <mark.padgham@email.com>
2024-01-23 10:54:30 +0100

    restructure transform page for metadata; closes #54

--- src/app/transform/page.tsx
+++ src/app/transform/page.tsx
@@ -1,67 +1,27 @@
-"use client"
 
-import Head from 'next/head'
+import type { Metadata, Viewport } from 'next';
 import Image from 'next/image'
-import { useState} from 'react';
-import { FlyToInterpolator } from "@deck.gl/core/typed";
-import Control from '@/components/maps/control';
-import LegendTransform from '@/components/maps/legend-transform';
 
-// import UTAMap from '@/components/maps/map';
-import MapTransformCalculate from '@/components/maps/map-transform';
-import styles from '@/styles/maps.module.css';
-import { CITY_DATA } from '@/data/citydata';
-import { ViewState } from "@/data/interfaces";
+export const metadata: Metadata = {
+    title: 'UA',
+    description: 'Maps for Urban Analyst',
+    icons: '/ua.ico',
+}
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1.0,
+    maximumScale: 1.0,
+    userScalable: false,
+}
+
+import MapTransformPage from '@/components/maps/mapTransformPage';
 
 export default function Home() {
 
-    const idx = 0;
-    const varnames = ["bike_index", "social_index"];
-    const nentries = 1000;
-    const mapPath = "/data/berlin/data.json";
-    const alpha = 0.7;
-
-    const layer_name = varnames[0];
-
-    const [viewState, setViewState] = useState({
-        ...CITY_DATA.citiesArray[idx].initialViewState,
-        pitch: 0,
-        bearing: 0,
-        transitionDuration: 2000,
-        transitionInterpolator: new FlyToInterpolator()
-    });
-
-    const [layerMin, setLayerMin] = useState<number>(0);
-    const [layerMax, setLayerMax] = useState<number>(0);
-    const handleLayerMinChange = (layerMin: number) => {
-        setLayerMin(layerMin);
-    }
-    const handleLayerMaxChange = (layerMax: number) => {
-        setLayerMax(layerMax);
-    }
-    const [layerName, setLayerName] = useState<string>("social_index");
-
     return (
         <>
-        <MapTransformCalculate
-            idx={idx}
-            varnames={varnames}
-            nentries={nentries}
-            mapPath={mapPath}
-            city = {CITY_DATA.citiesArray[idx].name}
-            viewState = {viewState}
-            alpha = {alpha}
-            layerMin={layerMin}
-            layerMax={layerMax}
-            handleLayerMinChange={handleLayerMinChange}
-            handleLayerMaxChange={handleLayerMaxChange}
-        />
-        <LegendTransform
-            layerMin={layerMin}
-            layerMax={layerMax}
-            alpha={alpha}
-            layer_name={layer_name}
-        />
+        <MapTransformPage />
         </>
     )
 }
---
+++ src/components/maps/mapTransformPage.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import Image from 'next/image'
+
+import { useState} from 'react';
+import { FlyToInterpolator } from "@deck.gl/core/typed";
+
+import Control from '@/components/maps/control';
+import LegendTransform from '@/components/maps/legend-transform';
+
+// import UTAMap from '@/components/maps/map';
+import MapTransformCalculate from '@/components/maps/map-transform';
+import styles from '@/styles/maps.module.css';
+import { CITY_DATA } from '@/data/citydata';
+import { ViewState } from "@/data/interfaces";
+
+export default function MapTransformPage() {
+
+    const idx = 0;
+    const varnames = ["bike_index", "social_index"];
+    const nentries = 1000;
+    const mapPath = "/data/berlin/data.json";
+    const alpha = 0.7;
+
+    const layer_name = varnames[0];
+
+    const [viewState, setViewState] = useState({
+        ...CITY_DATA.citiesArray[idx].initialViewState,
+        pitch: 0,
+        bearing: 0,
+        transitionDuration: 2000,
+        transitionInterpolator: new FlyToInterpolator()
+    });
+
+    const [layerMin, setLayerMin] = useState<number>(0);
+    const [layerMax, setLayerMax] = useState<number>(0);
+    const handleLayerMinChange = (layerMin: number) => {
+        setLayerMin(layerMin);
+    }
+    const handleLayerMaxChange = (layerMax: number) => {
+        setLayerMax(layerMax);
+    }
+    const [layerName, setLayerName] = useState<string>("social_index");
+
+    return (
+        <>
+        <MapTransformCalculate
+            idx={idx}
+            varnames={varnames}
+            nentries={nentries}
+            mapPath={mapPath}
+            city = {CITY_DATA.citiesArray[idx].name}
+            viewState = {viewState}
+            alpha = {alpha}
+            layerMin={layerMin}
+            layerMax={layerMax}
+            handleLayerMinChange={handleLayerMinChange}
+            handleLayerMaxChange={handleLayerMaxChange}
+        />
+        <LegendTransform
+            layerMin={layerMin}
+            layerMax={layerMax}
+            alpha={alpha}
+            layer_name={layer_name}
+        />
+        </>
+    )
+}
