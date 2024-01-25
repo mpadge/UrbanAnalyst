28b715de17d1e7f66861d28af14d1d1a17ebbf90
mpadge <mark.padgham@email.com>
2024-01-25 10:30:22 +0100

    reinstate transform page

---
+++ src/app/transform/page.tsx
@@ -0,0 +1,27 @@
+
+import type { Metadata, Viewport } from 'next';
+import Image from 'next/image'
+
+export const metadata: Metadata = {
+    title: 'UA',
+    description: 'Maps for Urban Analyst',
+    icons: '/ua.ico',
+}
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1.0,
+    maximumScale: 1.0,
+    userScalable: false,
+}
+
+import TransformPage from '@/components/transform/TransformPage';
+
+export default function Home() {
+
+    return (
+        <>
+        <TransformPage />
+        </>
+    )
+}
