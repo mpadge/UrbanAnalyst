59c5f42d4ea6fe13a94d2aa333bc3a1df9c932cd
mpadge <mark.padgham@email.com>
2024-02-22 13:40:11 +0100

    add tour skeleton to compare page for #84

---
+++ src/components/compare/tour/tourConfig.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import useWindowSize from '@/components/window-size';
+import { posControlsX, posControlsY, maxWidth } from "@/components/compare/tour/tourPositionControls";
+import { controlBoxText, legendText, navText } from "@/components/compare/tour/tourText";
+
+const tourPanelBackgroundColour = '#9cf7f7';
+const tourPanelBorderRadiusWide = '20px';
+const tourPanelBorderRadiusNarrow = '10px';
+
+export const getTourConfig = (width: number, height: number) => [
+    {    
+        selector: '[data-tut="reactour__compare_welcome"]',
+        content: () => (
+            <div>
+                <h2>Urban Analyst</h2>
+                <br />
+                This guided tour can be closed by clicking anywhere outside
+                these boxes, and re-started any time by clicking on the
+                <q>Help</q> button on the left.
+                <br />
+                <br />
+                If you&apos;ve got a keyboard, you can step through with arrow
+                keys, and close with <q>Escape</q>. Press an arrow now to
+                start...
+            </div>
+        ),
+        style: {
+            backgroundColor: tourPanelBackgroundColour,
+            borderRadius: width > 700 ? tourPanelBorderRadiusWide : tourPanelBorderRadiusNarrow,
+            maxWidth: maxWidth(width, true),
+        }
+    },
+    {
+        selector: '[data-tut="reactour__compare_page"]',
+        content: (
+            <div>
+                <h2>Maps</h2>
+                <br />
+                This page shows interactive maps for a chosen city, and for
+                values of a chosen variable or <q>layer</q>. Low values (yellow
+                colors) are generally better than higher values (blue colors).
+            </div>
+        ),
+        style: {
+            backgroundColor: tourPanelBackgroundColour,
+            borderRadius: width > 700 ? tourPanelBorderRadiusWide : tourPanelBorderRadiusNarrow,
+            left: posControlsX(width, "nav"),
+            width: maxWidth(width),
+        }
+    },
+    {
+        selector: '[data-tut="reactour__compare_controls"]',
+        content: () => controlBoxText(width > 700),
+        style: {
+            left: posControlsX(width, "controls"),
+            top: posControlsY(width, height, "controls"),
+            backgroundColor: tourPanelBackgroundColour,
+            borderRadius: width > 700 ? tourPanelBorderRadiusWide : tourPanelBorderRadiusNarrow,
+            maxWidth: maxWidth(width, true),
+        }
+    },
+    {    
+        selector: '[data-tut="reactour__compare_legend"]',
+        content: () => legendText(width > 700),
+        style: {
+            width: maxWidth(width),
+            left: posControlsX(width, "legend"),
+            top: posControlsY(width, height, "legend"),
+            backgroundColor: tourPanelBackgroundColour,
+            borderRadius: width > 700 ? tourPanelBorderRadiusWide : tourPanelBorderRadiusNarrow,
+        }
+    },
+    {
+        selector: '[data-tut="reactour__compare_nav_buttons"]',
+        style: {
+            backgroundColor: tourPanelBackgroundColour,
+            borderRadius: width > 700 ? tourPanelBorderRadiusWide : tourPanelBorderRadiusNarrow,
+            left: posControlsX(width, "nav"),
+            top: posControlsY(width, height, "nav"),
+            width: maxWidth(width),
+        },
+        content: ({ goTo }: { goTo: (stepIndex: number) => void }) => (
+            navText(goTo, width > 700)
+        )
+    },
+
+];
+
+export default getTourConfig;
