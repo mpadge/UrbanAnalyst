a83479af5b2bf287ecfc86aa6c2d44a8c70ae6a0
mpadge <mark.padgham@email.com>
2024-07-05 15:48:03 +0200

    add explain layer text to mui dialog for #112

--- src/components/map/explainButton.tsx
+++ src/components/map/explainButton.tsx
@@ -2,10 +2,13 @@ import { useState } from 'react';
 
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
 
 import styles from '@/styles/controls.module.css';
+import { GetLayerText } from "@/components/explainLayer";
 import { CityDataProps, ExplainButtonProps } from "@/data/interfaces";
 
 export interface MapsExplainProps {
@@ -27,9 +30,29 @@ function ExplainLayerDialog(props: MapsExplainProps) {
         onClose();
     };
 
+    const meanVarIndex = 0;
+    const paired_keys: string[] = Object.keys(props.citiesArray[props.idx].dataRangesPaired);
+
+    const layer1: string = props.layer.replace("\_", "").replace("index", "");
+    const layer2: string = props.layer2.replace("\_", "").replace("index", "");
+    const these_layers =
+        paired_keys.includes(layer1 + "_" + layer2) ?
+            layer1 + "_" + layer2 : layer2 + "_" + layer1;
+    const dual_layers: boolean = paired_keys.includes(these_layers);
+    const layer_temp = GetLayerText(props.layer, props.layer2, props.numLayers, meanVarIndex, paired_keys);
+    // But set layer text to city data "social_index" field if that single layer
+    // is selected:
+    const layer_text = !dual_layers && props.layer == "social_index" ?
+        props.citiesArray[props.idx].social_index : layer_temp;
+
     return (
         <Dialog onClose={handleClose} open={open}>
             <DialogTitle>Set backup account</DialogTitle>
+                    <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    {layer_text}
+                </DialogContentText>
+            </DialogContent>
         </Dialog>
     );
 }
