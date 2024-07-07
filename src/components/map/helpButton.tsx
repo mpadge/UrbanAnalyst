b2d34774d703c77c745f65149498a421d8c6e5fb
mpadge <mark.padgham@email.com>
2024-07-07 10:10:00 +0200

    rm unused help button pars from maps page #112

--- src/components/map/helpButton.tsx
+++ src/components/map/helpButton.tsx
@@ -4,17 +4,12 @@ import { useState } from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 
-import styles from '@/styles/controls.module.css';
-import { ExplainButtonProps } from "@/data/interfaces";
-import tourStyles from '@/styles/tour.module.css';
-
 interface HelpButtonProps {
     handleTourOpen: (isTourOpen: boolean) => void
 }
 
 export default function HelpButton (props: HelpButtonProps) {
 
-    var buttonStyle: any = styles.explainbuttonOff;
     var buttonText: string = "Help";
 
     return (
