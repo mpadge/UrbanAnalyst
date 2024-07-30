a75eb6a2c0cc69e371c1848798e5928ed72868d7
mpadge <mark.padgham@email.com>
2024-07-30 16:03:10 +0200

    tsdoc map.tsx for #121

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -14,6 +14,14 @@ const MapboxAccessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 // const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
 const MAP_STYLE = "mapbox://styles/mapbox/light-v10"
 
+/**
+ * Map function to load DeckGL overlap and base map.
+ *
+ * The only state variable constructed here is `layer`, which is necessary
+ * because this is returned from the `mapLayer` function defined in
+ * `mapLayer.tsx`. That function must first be called to obtain the layer before
+ * passing to final DOM elements in return body of this function.
+ */
 export default function UTAMap (props: MapProps) {
 
     const this_layer = mapLayer(props);
