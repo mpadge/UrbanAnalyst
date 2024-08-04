6e60cc52f30cc092734312ae05895a63954943ee
mpadge <mark.padgham@email.com>
2024-08-04 15:56:46 +0200

    make appbar semi-transparent #138

--- src/components/appBar.tsx
+++ src/components/appBar.tsx
@@ -19,9 +19,9 @@ const theme = createTheme({
   palette: {
     primary: {
       light: '#757ce8',
-      main: '#3f50b5',
+      main: '#3f50b555',
       dark: '#002884',
-      contrastText: '#fff',
+      contrastText: '#000',
     },
     secondary: {
       light: '#ff7961',
