fad62b1e616b4c41979a695d33819f7f1316178a
mpadge <mark.padgham@email.com>
2024-08-04 15:02:30 +0200

    tweak appbar logo for #118

--- src/components/uaSvgLogo.tsx
+++ src/components/uaSvgLogo.tsx
@@ -12,32 +12,32 @@ export const SvgComponent: React.FC<React.SVGProps<SVGSVGElement>> = (props) => 
         <path
             fill="transparent"
             stroke="currentColor"
-            strokeWidth="3"
+            strokeWidth="4"
             d="M 0,5 v -5 h 24"
         />
         <path
             fill="transparent"
             stroke="currentColor"
-            strokeWidth="3"
+            strokeWidth="4"
             d="M 24,19 v 5 h -24"
         />
         <path
             fill="transparent"
             stroke="currentColor"
-            strokeWidth="2"
-            d="M 3 6 v 8 C 3 21, 11 21, 11 14 v -8"
+            strokeWidth="3"
+            d="M 4 6 v 8 C 4 21, 10 21, 10 14 v -8"
         />
         <path
             fill="transparent"
             stroke="currentColor"
-            strokeWidth="2"
-            d="M 13 20 l 5 -14 l 5 14"
+            strokeWidth="3"
+            d="M 13 20 l 4 -12 l 4 12"
         />
         <path
             fill="transparent"
             stroke="currentColor"
-            strokeWidth="2"
-            d="M 14 15 h 9"
+            strokeWidth="3"
+            d="M 15 15 h 5"
         />
   </svg>
 )
