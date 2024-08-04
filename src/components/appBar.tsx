3e5ed208ba69a702bbdde6258685d1a2cdb34506
mpadge <mark.padgham@email.com>
2024-08-04 13:53:04 +0200

    rough mui svgicon on appbar for #118

--- src/components/appBar.tsx
+++ src/components/appBar.tsx
@@ -11,9 +11,53 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import SvgIcon from '@mui/material/SvgIcon';
 
 import useWindowSize from "@/components/windowSize";
 import { ButtonAppProps } from '@/data/interfaces';
+
+
+const SvgComponent = (props) => (
+  <svg
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        {...props}
+    >
+        <path
+            fill="transparent"
+            stroke="currentColor"
+            strokeWidth="3"
+            d="M 0,5 v -5 h 24"
+        />
+        <path
+            fill="transparent"
+            stroke="currentColor"
+            strokeWidth="3"
+            d="M 24,19 v 5 h -24"
+        />
+        <path
+            fill="transparent"
+            stroke="currentColor"
+            strokeWidth="2"
+            d="M 3 6 v 8 C 3 21, 11 21, 11 14 v -8"
+        />
+        <path
+            fill="transparent"
+            stroke="currentColor"
+            strokeWidth="2"
+            d="M 13 20 l 5 -14 l 5 14"
+        />
+        <path
+            fill="transparent"
+            stroke="currentColor"
+            strokeWidth="2"
+            d="M 14 15 h 9"
+        />
+  </svg>
+)
+
+import UAIcon from "../../public/ua.svg";
 
 function getHref(s: string) {
     if (s == "home") {
@@ -88,6 +132,8 @@ export default function ButtonAppBar(props: ButtonAppProps) {
                         </Menu>
                     </Box>
 
+                    <SvgIcon component={SvgComponent}>
+                    </SvgIcon>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         UA
                     </Typography>
