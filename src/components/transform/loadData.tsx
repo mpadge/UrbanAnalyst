6769c77df8577971a7a430143dc794553d8acddb
mpadge <mark.padgham@email.com>
2026-02-02 13:43:42 +0100

    mv api/gh -> api/ghHiResData for #190

--- src/components/transform/loadData.tsx
+++ src/components/transform/loadData.tsx
@@ -1,5 +1,5 @@
 export async function loadDataFunction(city: string, handleDataChange: (data: number) => void) {
-    const response = await fetch(`/api/gh?city=${city}`);
+    const response = await fetch(`/api/ghHiResData?city=${city}`);
     if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
     }
